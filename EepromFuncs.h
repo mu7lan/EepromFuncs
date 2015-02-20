b8b2ef3b89bdd0c316432424d5319c085eb4e5db
David <disco47dave@gmail.com>
2015-02-19 18:03:31 -0600

    First Commit

---
+++ EepromFuncs.h
@@ -0,0 +1,46 @@
+#ifndef EepromFuncs_h
+#define EepromFuncs_h
+
+
+#include <EEPROM.h>
+
+
+template<class T>
+int writeToEEPROM(int address, T& value)
+{
+  union {
+    T type;
+    byte b[sizeof(T)];
+  } 
+  temp;
+
+  temp.type = value;
+  for (unsigned int i = 0; i < sizeof(T); i++)
+  {
+    EEPROM.write(address + i, temp.b[i]);
+  }
+  return sizeof(T);
+}
+
+
+template<class T>
+int readFromEEPROM(int address, T& value)
+{
+  union {
+    T type;
+    byte b[sizeof(T)];
+  } 
+  temp;
+
+  for (unsigned int i = 0; i < sizeof(T); i++)
+  {
+    temp.b[i] = EEPROM.read(address + i);
+  }
+  value = temp.type;
+  return sizeof(T);
+}
+
+
+
+
+#endif
